dce41eb9e1981ab250b949785c1ef0b752278933
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2023-11-20 18:52:26 +0800

    Add solution for exercises 6.14 to 6.15

--- part6/anecdotes-redux/src/App.jsx
+++ part6/anecdotes-redux/src/App.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
 import AnecdoteList from "./components/AnecdoteList";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Filter from "./components/Filter";
 import Notification from "./components/Notification";
 
+import anecdoteService from "./services/anecdotes";
+import { setAnecdotes } from "./reducers/anecdoteReducer";
+import { useDispatch } from "react-redux";
+
 const App = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        anecdoteService.getAll().then((notes) => dispatch(setAnecdotes(notes)));
+    }, []);
+
     return (
         <div>
             <Notification />
--- part6/anecdotes-redux/src/components/Notification.jsx
+++ part6/anecdotes-redux/src/components/Notification.jsx
@@ -7,7 +7,11 @@ const Notification = () => {
         padding: 10,
         borderWidth: 1,
     };
-    return notification && <div style={style}>{notification}</div>;
+    return (
+        notification && (
+            <div style={style}>{`You have voted "${notification}".`}</div>
+        )
+    );
 };
 
 export default Notification;
