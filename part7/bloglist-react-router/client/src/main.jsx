2b5f39831d37e4a78deefe5110bb8b13428a8e75
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2023-11-28 19:43:52 +0800

    Initial react-router setup

--- part7/bloglist-react-router/client/src/main.jsx
+++ part7/bloglist-react-router/client/src/main.jsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-// import { Router } from "react-router-dom";
+import { Router } from "react-router-dom";
 import store from "./store";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
-        {/* <Router> */}
-        <App />
-        {/* </Router> */}
+        <Router>
+            <App />
+        </Router>
     </Provider>,
 );
