cccf62bac9e8193be2947fd6961f3c3a2d4a85d5
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2023-12-07 19:29:54 +0800

    Add solution to exercise 8.17

--- part8/library-frontend/src/components/Books.jsx
+++ part8/library-frontend/src/components/Books.jsx
@@ -24,7 +24,7 @@ const Books = () => {
                     {books.map((a) => (
                         <tr key={a.title}>
                             <td>{a.title}</td>
-                            <td>{a.author}</td>
+                            <td>{a.author.name}</td>
                             <td>{a.published}</td>
                         </tr>
                     ))}
