27af1a51ecf5b0670ec3770e30d81df77adaf2dd
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2023-12-11 17:44:14 +0800

    Add solution for exercise 9.10

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import diagnosesRouter from "./routes/diagnosesRoutes";
 const app = express();
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.get("/ping", (_req, res) => {
     console.log("someone pinged here");
     res.send("pong");
 });
+
+app.use("/api/diagnoses", diagnosesRouter);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
---
+++ part9/patientor-backend/src/services/diagnosesService.ts
@@ -0,0 +1,11 @@
+import diagnosesData from "../../data/diagnoses";
+
+import { DiagnosesEntry } from "../types";
+
+const getDiagnoses = (): DiagnosesEntry[] => {
+    return diagnosesData;
+};
+
+export default {
+    getDiagnoses,
+};
