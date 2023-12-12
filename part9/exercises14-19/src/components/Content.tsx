07cb95ff2e733dbb1f0ecdd86344455dd23ffdd5
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2023-12-12 18:45:23 +0800

    Add solution for exercise 9.15

--- part9/exercises14-19/src/components/Content.tsx
+++ part9/exercises14-19/src/components/Content.tsx
@@ -1,12 +1,87 @@
 import { CoursePart } from "../utils/types";
 
 const Content = (props: { courseParts: CoursePart[] }) => {
+    const assertNever = (value: never): never => {
+        throw new Error(
+            `Unhandled discriminated union member: ${JSON.stringify(value)}`
+        );
+    };
+
     return (
         <>
             {props.courseParts.map((part, index) => (
-                <p key={index}>
-                    {part.name} {part.exerciseCount}
-                </p>
+                <div key={index}>
+                    {(() => {
+                        switch (part.kind) {
+                            case "basic":
+                                return (
+                                    <div>
+                                        <p>
+                                            <b>
+                                                {part.name} -{" "}
+                                                {part.exerciseCount}
+                                            </b>
+                                        </p>
+                                        <p>
+                                            <i>{part.description}</i>
+                                        </p>
+                                    </div>
+                                );
+                            case "group":
+                                return (
+                                    <div>
+                                        <p>
+                                            <b>
+                                                {part.name} -{" "}
+                                                {part.exerciseCount}
+                                            </b>
+                                        </p>
+                                        <p>
+                                            Project Exercises:{" "}
+                                            {part.groupProjectCount}
+                                        </p>
+                                    </div>
+                                );
+                            case "background":
+                                return (
+                                    <div>
+                                        <p>
+                                            <b>
+                                                {part.name} -{" "}
+                                                {part.exerciseCount}
+                                            </b>
+                                        </p>
+                                        <p>
+                                            <i>{part.description}</i>
+                                        </p>
+                                        <p>
+                                            Submit to: {part.backgroundMaterial}
+                                        </p>
+                                    </div>
+                                );
+                            case "special":
+                                return (
+                                    <div>
+                                        <p>
+                                            <b>
+                                                {part.name} -{" "}
+                                                {part.exerciseCount}
+                                            </b>
+                                        </p>
+                                        <p>
+                                            <i>{part.description}</i>
+                                        </p>
+                                        <p>
+                                            Required Skills:{" "}
+                                            {part.requirements.join(", ")}
+                                        </p>
+                                    </div>
+                                );
+                            default:
+                                return assertNever(part);
+                        }
+                    })()}
+                </div>
             ))}
         </>
     );
--- part9/exercises14-19/src/utils/types.tsx
+++ part9/exercises14-19/src/utils/types.tsx
@@ -2,7 +2,36 @@ export interface HeaderProps {
     courseName: string;
 }
 
-export interface CoursePart {
+interface CoursePartBase {
     name: string;
     exerciseCount: number;
 }
+
+interface CoursePartBaseWithBackground extends CoursePartBase {
+    description: string;
+}
+
+interface CoursePartBasic extends CoursePartBaseWithBackground {
+    kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+    groupProjectCount: number;
+    kind: "group";
+}
+
+interface CoursePartBackground extends CoursePartBaseWithBackground {
+    backgroundMaterial: string;
+    kind: "background";
+}
+
+interface CoursePartSpecial extends CoursePartBaseWithBackground {
+    requirements: string[];
+    kind: "special";
+}
+
+export type CoursePart =
+    | CoursePartBasic
+    | CoursePartGroup
+    | CoursePartBackground
+    | CoursePartSpecial;
