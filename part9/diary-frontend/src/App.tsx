97222006c2a3a337b6b51064289aa23a23dad1df
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2023-12-14 02:19:20 +0800

    Add solution for exercise 9.17

--- part9/diary-frontend/src/App.tsx
+++ part9/diary-frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
-import { NonSensitiveDiaryEntry } from "./types";
-import { getAllEntries } from "./diaryService";
+import {
+    NonSensitiveDiaryEntry,
+    NewDiaryEntry,
+    Weather,
+    Visibility,
+} from "./types";
+import { getAllEntries, createEntry } from "./diaryService";
 
 const App = () => {
     const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([]);
-    // const [newDate, setNewDate] = useState("");
-    // const [newWeather, setNewWeather] = useState("");
-    // const [newVisibility, setNewVisibility] = useState("");
+    const [newDate, setNewDate] = useState("");
+    const [newWeather, setNewWeather] = useState("");
+    const [newVisibility, setNewVisibility] = useState("");
 
     useEffect(() => {
         getAllEntries().then((data) => {
@@ -14,22 +19,85 @@ const App = () => {
         });
     }, []);
 
-    const diaryCreation = (event: React.SyntheticEvent) => {
-        event.preventDefault();
-        // createEntry({ content: newNote }).then((data) => {
-        //     setNotes(notes.concat(data));
-        // });
+    const getWeatherEnum = (weather: string): Weather => {
+        switch (weather.toLowerCase()) {
+            case "sunny":
+                return Weather.Sunny;
+            case "rainy":
+                return Weather.Rainy;
+            case "cloudy":
+                return Weather.Cloudy;
+            case "stormy":
+                return Weather.Stormy;
+            case "windy":
+                return Weather.Windy;
+            default:
+                throw new Error("Invalid weather value");
+        }
+    };
 
-        // setNewNote("");
+    const getVisibilityEnum = (visibility: string): Visibility => {
+        switch (visibility.toLowerCase()) {
+            case "great":
+                return Visibility.Great;
+            case "good":
+                return Visibility.Good;
+            case "ok":
+                return Visibility.Ok;
+            case "poor":
+                return Visibility.Poor;
+            default:
+                throw new Error("Invalid visibility value");
+        }
+    };
+
+    const diaryCreation = async (event: React.SyntheticEvent) => {
+        event.preventDefault();
+
+        const newEntry: NewDiaryEntry = {
+            date: newDate,
+            weather: getWeatherEnum(newWeather),
+            visibility: getVisibilityEnum(newVisibility),
+            comment: "",
+        };
+
+        try {
+            const createdEntry = await createEntry(newEntry);
+            setDiaries(diaries.concat(createdEntry));
+            setNewDate("");
+            setNewWeather("");
+            setNewVisibility("");
+        } catch (error) {
+            console.error("Error creating entry:", error);
+        }
     };
 
     return (
         <div>
             <form onSubmit={diaryCreation}>
-                {/* <input
-                    value={newDate}
-                    onChange={(event) => setNewDate(event.target.value)}
-                /> */}
+                <div>
+                    <label>Date: </label>
+                    <input
+                        value={newDate}
+                        onChange={(event) => setNewDate(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Weather: </label>
+                    <input
+                        value={newWeather}
+                        onChange={(event) => setNewWeather(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>Visibility: </label>
+                    <input
+                        value={newVisibility}
+                        onChange={(event) =>
+                            setNewVisibility(event.target.value)
+                        }
+                    />
+                </div>
                 <button type="submit">add</button>
             </form>
             <div>
