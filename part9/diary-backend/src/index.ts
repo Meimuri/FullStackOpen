d8502b325038fc5d689bb54738b7d81db057ff0b
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2023-12-14 01:57:36 +0800

    Add cors

--- part9/diary-backend/src/index.ts
+++ part9/diary-backend/src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
-const app = express();
+import cors from "cors";
+
 import diaryRouter from "./routes/diaries";
+
+const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3000;
 
