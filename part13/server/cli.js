eefc0a8f59f752be7bf9ad9957e7952d3698750d
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2023-12-30 14:39:00 +0800

    Add solution for exercise 13.3

---
+++ part13/server/cli.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const { Sequelize, QueryTypes } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {},
+});
+
+const main = async () => {
+    try {
+        await sequelize.authenticate();
+        const notes = await sequelize.query("SELECT * FROM blogs", {
+            type: QueryTypes.SELECT,
+        });
+        console.log(notes);
+        sequelize.close();
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+};
+
+main();
