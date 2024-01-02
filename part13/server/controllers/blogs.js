2002de6583506b34bf3cb380cf1a9ffaba77ee76
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2024-01-02 23:05:43 +0800

    Add solution for exercise 13.14

--- part13/server/controllers/blogs.js
+++ part13/server/controllers/blogs.js
@@ -12,9 +12,18 @@ router.get("/", async (req, res) => {
     const where = {};
 
     if (req.query.search) {
-        where.title = {
-            [Op.substring]: req.query.search,
-        };
+        where[Op.or] = [
+            {
+                title: {
+                    [Op.substring]: req.query.search,
+                },
+            },
+            {
+                author: {
+                    [Op.substring]: req.query.search,
+                },
+            },
+        ];
     }
 
     const blogs = await Blog.findAll({
