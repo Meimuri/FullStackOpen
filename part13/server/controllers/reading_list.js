8a42038c785d1c398801c5f3dcc58af06982017f
Michael A. Alcaraz II <22783799+Meimuri@users.noreply.github.com>
2024-01-05 00:30:17 +0800

    Add solution for exercise 13.22

--- part13/server/controllers/reading_list.js
+++ part13/server/controllers/reading_list.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { UserReadingList } = require("../models");
 
+const { userExtractor } = require("../util/middleware");
+
 router.get("/", async (req, res) => {
     const readingList = await UserReadingList.findAll();
     res.json(readingList);
@@ -9,6 +11,24 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
     const savedReadingList = await UserReadingList.create(req.body);
     res.status(201).json(savedReadingList);
+});
+
+router.put("/:id", userExtractor, async (req, res) => {
+    const readingList = await UserReadingList.findByPk(req.params.id);
+    if (!readingList) {
+        return res.status(404).json({ error: "Reading List not found" });
+    } else {
+        if (readingList.id === req.user.id) {
+            readingList.read = !readingList.read;
+            await readingList.save();
+
+            res.status(200).json(readingList);
+        } else {
+            res.status(401).json({
+                error: "You cannot update reading list that isn't yours",
+            });
+        }
+    }
 });
 
 module.exports = router;
